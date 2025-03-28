a016343b741132e6616b778e8241d9f36f503342
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-03-28 21:59:17 +0100

    omo

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    // req.header("Content-Type: application/x-www-form-urlencoded");
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Methods", " GET, POST, OPTIONS, PUT, PATCH, DELETE")
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token");
+    req.header("Content-Type: application/x-www-form-urlencoded");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", " GET, POST, OPTIONS, PUT, PATCH, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token");
 
     res.header(
         'Access-Control-Expose-Headers',
